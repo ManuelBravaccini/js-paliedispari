9d5691d9b534a531efec60f407b9c5db304f8235
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-11-30 21:29:11 +0100

    Implemented script oddoreven mini-game, and reworked function checkPalindromeWord

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const userWord = prompt("Immetti una parola");
 
-function checkPalindromeWord(userWord) {
+function checkPalindromeWord(word) {
     
     const length = userWord.length;
 
     for (let i = 0; i < length / 2; i++) {
 
-        if (userWord[i] !== userWord[length - 1 - i]) {
+        if (word[i] !== word[length - 1 - i]) {
             return "Non è palindroma!";
         }
     }
@@ -16,3 +16,32 @@ function checkPalindromeWord(userWord) {
 const result = checkPalindromeWord(userWord);
 
 console.log(result);
+
+
+function oddOrEven(num) {
+    if (num % 2 == 0){
+        return "pari";
+    } 
+    return "dispari";
+}
+
+function getRandomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+} 
+
+const userChoice = prompt("Pari o dispari?").toLocaleLowerCase().trim();
+console.log("L'utente ha scelto " + userChoice);
+
+const userNumber = parseInt(prompt("Scegli un numero"), 10);
+console.log("L'utente ha scelto " + userNumber);
+
+const pcNumber = getRandomNumber(1, 6);
+console.log("Il PC ha scelto " + pcNumber);
+
+const sum = userNumber + pcNumber;  
+
+if (oddOrEven(sum) === userChoice){
+    console.log("Hai vinto");
+} else {
+    console.log("Non hai vinto");
+}
